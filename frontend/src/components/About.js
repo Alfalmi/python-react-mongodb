950078e58016667fd70a4b68e62bc3436d93f12c
Alfalmi <pinooxd@gmail.com>
2021-09-01 07:42:10 -0700

    navbar

---
+++ frontend/src/components/About.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+
+export const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <p>
+      Ut quia id distinctio voluptatem. Quas corrupti repellat autem aut. Sequi
+      et sed eum. Consequatur expedita quo et quod sint exercitationem aut qui.
+      Autem aliquid beatae facilis facilis delectus aliquam inventore voluptas
+      nemo.
+    </p>
+    <p>
+      Dolore exercitationem voluptatem ut et soluta vel et. Eum optio libero
+      dolores ducimus cum. Dolor modi debitis similique. Atque optio voluptate
+      perferendis dolor itaque in ad. Aut debitis in molestias facere quia.
+      Numquam minus iusto. Quas modi iure quo aliquid. Quis itaque consequuntur.
+      At suscipit ratione recusandae dolore. Alias molestiae dolores enim sit
+      quo at aliquid necessitatibus. Pariatur fugiat et voluptas voluptatem.
+      Explicabo et vitae eum omnis. Voluptate omnis in omnis sunt saepe maiores
+      similique qui unde.
+    </p>
+    <p>
+      Tempore cum placeat commodi alias sit quaerat ut dolorem quasi. Tempore
+      quisquam commodi. Neque sapiente iusto maxime occaecati. Facere ut est.
+      Vel est id quia quis porro aut totam. Voluptatem esse aut nemo doloribus
+      aut facere pariatur molestias debitis. Natus laborum eveniet possimus
+      tempore quae nemo. Dolores iusto veritatis est quis nostrum sed accusamus
+      recusandae. Culpa ut voluptatem vel. Expedita eum odit.
+    </p>
+    <p>
+      In sapiente omnis totam non libero voluptatum totam explicabo. Nesciunt
+      illo doloremque. Cumque eaque maxime minus repellat maiores. Voluptas ut
+      sed autem veniam adipisci ea dolor et. Vel necessitatibus soluta. Nihil
+      minus quae sequi rem optio fugit ratione enim. Rerum sit exercitationem
+      aut dolorum enim. Sed unde necessitatibus consequatur quia. Veniam
+      dignissimos porro consectetur assumenda commodi quaerat reiciendis itaque.
+      Tempore laborum quod voluptatum quibusdam quia voluptatibus eveniet quo
+      consequatur. Sed numquam perferendis doloribus fugit at consequatur porro
+      dolorum fugit. Dolor dignissimos quo. Rerum quod sunt.
+    </p>
+    <p>
+      Iure porro quo perspiciatis vel. Voluptatum dolor et et vitae. Voluptate
+      non est. Facere nesciunt et quo quam quo accusantium nobis. Impedit aut
+      cumque vel laborum non officiis. Sapiente expedita magnam modi. Quos
+      reiciendis similique in eum et. Vel hic aliquam ratione et. Quis quia sit.
+      Quia unde odio neque rerum est laborum rerum culpa. Eum iusto quos sed
+      reiciendis vero iusto in. Delectus facere non hic aut commodi. Earum hic
+      enim. Repellendus est quis velit molestiae consequatur.
+    </p>
+  </Fragment>
+);
