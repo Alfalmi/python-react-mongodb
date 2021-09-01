17209058208b5ff10dd7462ff2c91c3cd8287679
Alfalmi <pinooxd@gmail.com>
2021-09-01 09:22:52 -0700

    get

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { About } from "./components/About";
-import Users from "./components/Users";
+import { Users } from "./components/Users";
 import { Navbar } from "./components/Navbar";
 
 function App() {
--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const Users = () => {
-    return (
-        <div>
-            Users
-        </div>
-    )
-}
+const baseURL = process.env.REACT_APP_API_URL
 
-export default Users
+export const Users = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${baseURL}/users`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      })
+    })
+    const data = await res.json();
+    console.log(data)
+  };
+  const getUsers = async () => {
+      const res = await fetch(`${baseURL}/users`)
+      const data = await res.json();
+      console.log(data)
+
+  }
+  useEffect(() => {
+      getUsers();
+  }, [])
+  return (
+    <div className="row">
+      <div className="col-md-4">
+        <form onSubmit={handleSubmit} className="card card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="form-control"
+              placeholder="Name"
+              autoFocus
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="form-control"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <button className="btn btn-primary btn-block">CREATE</button>
+        </form>
+      </div>
+      <div className="col-md-8"></div>
+    </div>
+  );
+};
